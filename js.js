fd875395f7d265e4013c751e0a9b6d31ecec2762
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-07 19:47:58 +0300

    fixed

--- js.js
+++ js.js
@@ -2,28 +2,29 @@
 
 const pass = prompt("Введите пароль", '');
 
-const arrayFromPass = pass.split ('') // Массив из строки
 let isUpperCase = 0  // Переменная для большой буквы
 let isNumber = 0	// Переменная для цифры
 
-for (const word of arrayFromPass) {
+
+
+for (const word of pass) {
 	
 	
 	
-	if (Number(word)) {    // Если число то в "большую букву" плюсуется 0
-		isUpperCase += 0
+	if ( !Number(word) && word === word.toUpperCase()) {    // Если число то в "большую букву" плюсуется 0
+		isUpperCase += 1
 	}
-	else if (word === word.toUpperCase()) { // Если верхний регистр то +1
- 		isUpperCase +=1
-	}
+	
 	
 
-	if (Number(word) >= 0 && Number(word) <= 9 && Number(word) !== NaN) {  
+	if (Number(word) && word !== " ") {  
 		isNumber += 1
 	}
 }
 
-if (!pass.includes(" ") && pass.length >= 3 && pass.length <= 30 && isUpperCase >= 1 && isNumber >= 1) {   // Если итоговая сумма переменных для большой буквы и цифры больше 0, то пароль валидный
+
+
+if (pass.length > 3 && pass.length <= 30 && isUpperCase >= 1 && isNumber >= 1) {   // Если итоговая сумма переменных для большой буквы и цифры больше 0, то пароль валидный
 																					
 	alert('Пароль валидный. Добро пожаловать в аккаунт!')
 
