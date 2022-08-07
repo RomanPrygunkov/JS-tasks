6b7f18e104cbe5fe593737ef35aaba84f1a05818
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-07 19:05:25 +0300

    fixed Null string and deleted array "item"

--- js.js
+++ js.js
@@ -3,13 +3,12 @@
 const pass = prompt("Введите пароль", '');
 
 const arrayFromPass = pass.split ('') // Массив из строки
-const item = [] // Push символов из For
 let isUpperCase = 0  // Переменная для большой буквы
 let isNumber = 0	// Переменная для цифры
 
 for (const word of arrayFromPass) {
 	
-	item.push(word)  // Пушим в массив item
+	
 	
 	if (Number(word)) {    // Если число то в "большую букву" плюсуется 0
 		isUpperCase += 0
@@ -24,7 +23,7 @@ for (const word of arrayFromPass) {
 	}
 }
 
-if (pass.length >= 3 && pass.length <= 30 && isUpperCase >= 1 && isNumber >= 1) {   // Если итоговая сумма переменных для большой буквы и цифры больше 0, то пароль валидный
+if (pass!== '' && pass.length >= 3 && pass.length <= 30 && isUpperCase >= 1 && isNumber >= 1) {   // Если итоговая сумма переменных для большой буквы и цифры больше 0, то пароль валидный
 																					
 	alert('Пароль валидный. Добро пожаловать в аккаунт!')
 
