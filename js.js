719ca03a28fbe2df4567d692b48a9b9d77706bcf
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-07 16:17:02 +0300

    Task #1

---
+++ js.js
@@ -0,0 +1,34 @@
+
+
+const pass = prompt("Введите пароль", '');
+
+const arrayFromPass = pass.split ('') // Массив из строки
+const item = [] // Push символов из For
+let isUpperCase = 0  // Переменная для большой буквы
+let isNumber = 0	// Переменная для цифры
+
+for (const word of arrayFromPass) {
+	
+	item.push(word)  // Пушим в массив item
+	
+	if (Number(word)) {    // Если число то в "большую букву" плюсуется 0
+		isUpperCase += 0
+	}
+	else if (word === word.toUpperCase()) { // Если верхний регистр то +1
+ 		isUpperCase +=1
+	}
+	
+
+	if (Number(word) >= 0 && Number(word) <= 9 && Number(word) !== NaN) {  
+		isNumber += 1
+	}
+}
+
+if (pass.length >= 3 && pass.length <= 30 && isUpperCase >= 1 && isNumber >= 1) {   // Если итоговая сумма переменных для большой буквы и цифры больше 0, то пароль валидный
+																					
+	alert('Пароль валидный. Добро пожаловать в аккаунт!')
+
+} else {
+	alert('Пароль не удовлетворяет условиям! Перезагрузите страницу и попробуйте ввести его еще раз.')
+	}
+
